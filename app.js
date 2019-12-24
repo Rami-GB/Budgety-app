0fbd353705f3aa6181b9d24bba6933db5dc2482f
ramigb <ramigoual@gmail.com>
2019-12-24 13:49:57 +0100

    creat todo list and event handlers

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
-var budgetController = (function() {
+
+//BUDGET LOGIC
+var budgetController = (function () {
     //code
 
     return {
-        
+
     }
 })();
 
 
-
-var UIController = (function() {
+//UI LOGIC
+var UIController = (function () {
     //code
 
     return {
-        
+
     }
 })();
 
 
-
-var controller = (function(budgetCtrl, UICtrl) {
+//GLOBAL CONTRLLER
+var controller = (function (budgetCtrl, UICtrl) {
     //code
+    var ctrlAddItem = function () {
+        //1. get input data
 
-    return {
-        
-    }
+        //2. add to budget ctrl
+
+        //3. add to UI
+
+        //4. calculate budget
+
+        //5. display budget on UI
+        console.log('hello world!');
+    };
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (e) {
+        if(e.which === 13  ||  e.keyCode === 13) {
+            ctrlAddItem();
+        }
+    });
+
 })(budgetController, UIController);
