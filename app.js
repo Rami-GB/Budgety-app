5b6da4dcd954e19ae5e3ea023efdca47aa637e3d
ramigb <ramigoual@gmail.com>
2019-12-24 14:16:29 +0100

    implementing getInput method of UIctrl

--- app.js
+++ app.js
@@ -12,9 +12,27 @@ var budgetController = (function () {
 //UI LOGIC
 var UIController = (function () {
     //code
+    var  DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+
 
     return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            }
+        },
 
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
     }
 })();
 
@@ -22,9 +40,13 @@ var UIController = (function () {
 //GLOBAL CONTRLLER
 var controller = (function (budgetCtrl, UICtrl) {
     //code
+    var DOMs = UICtrl.getDOMstrings();
+
+
     var ctrlAddItem = function () {
         //1. get input data
-
+        var input = UICtrl.getInput();
+        console.log(input);
         //2. add to budget ctrl
 
         //3. add to UI
@@ -35,12 +57,10 @@ var controller = (function (budgetCtrl, UICtrl) {
         console.log('hello world!');
     };
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOMs.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (e) {
-        if(e.which === 13  ||  e.keyCode === 13) {
-            ctrlAddItem();
-        }
+        if (e.which === 13 || e.keyCode === 13) { ctrlAddItem(); }
     });
 
 })(budgetController, UIController);
