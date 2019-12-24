77a893c232d6a6e0c47d3438972d73fb0da9b3ab
ramigb <ramigoual@gmail.com>
2019-12-24 13:06:01 +0100

    modules setup

---
+++ app.js
@@ -0,0 +1,27 @@
+var budgetController = (function() {
+    //code
+
+    return {
+        
+    }
+})();
+
+
+
+var UIController = (function() {
+    //code
+
+    return {
+        
+    }
+})();
+
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+    //code
+
+    return {
+        
+    }
+})(budgetController, UIController);
